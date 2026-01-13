f08c542beb1bb13b82879e066147116722e92014
Rishav <solarisxavier@gmail.com>
2026-01-14 02:03:47 +0530

    feat : modified admin form login

--- components/Admin/AdminInputForm.tsx
+++ components/Admin/AdminInputForm.tsx
@@ -12,8 +12,8 @@ interface propType {
 }
 
 export default function AdminInputForm({ setQuestions }: propType) {
-    
-    const[closeSubmit,setCloseSubmit] = useState<boolean>(true)
+
+    const [closeSubmit, setCloseSubmit] = useState<boolean>(true)
 
     const [question, setQuestion] = useState<questionType>({
         questionName: "",
@@ -26,21 +26,21 @@ export default function AdminInputForm({ setQuestions }: propType) {
             if (!prev) {
                 return [question]
             }
-            return[...prev , question]
+            return [...prev, question]
         })
-        
-        alert("Thank You For Submitting Your question")
+
+        // alert("Thank You For Submitting Your question")
         setQuestion({ ...question, questionName: "", answerOptions: [""], correctAnswer: "" })
     }
 
     useEffect(() => {
-        if (question.questionName.length > 8 && question.answerOptions.length > 1 && question.correctAnswer.length > 0) {
+        if (question.questionName.length > 8 && question.answerOptions.length > 1 && question.correctAnswer.length > 0 && Number(question.correctAnswer) <= question.answerOptions.length && Number(question.correctAnswer) > 0) {
             setCloseSubmit(false)
         }
         else {
             setCloseSubmit(true)
         }
-    },[question])
+    }, [question])
 
     console.log(question);
 
@@ -61,7 +61,7 @@ export default function AdminInputForm({ setQuestions }: propType) {
                                 <li className='w-full parent_cont flex flex-col gap-3 mt-4'>
                                     <label htmlFor="optionText1" className='text-black font-semibold text-sm '>Option {i + 1}</label>
                                     <input type="text" name="optionText1" id="" className='outline-1 h-10 w-full outline-black/50 rounded-lg pl-4'
-                                        
+
                                         onChange={(e) => {
                                             const value = e.target.value;
 
@@ -82,16 +82,23 @@ export default function AdminInputForm({ setQuestions }: propType) {
                     </ul>
                     <div className='flex gap-4 mt-3 justify-end flex-row '>
                         <p className='text-blue-600 add_button font-medium font-sora cursor-pointer' onClick={() => { setQuestion(prev => ({ ...prev, answerOptions: [...prev.answerOptions, " "] })) }}>Add</p>
-                        <p className='text-red-600 delete_button font-medium font-sora cursor-pointer' onClick={()=>{setQuestion(prev => ({...prev,  answerOptions: prev.answerOptions.slice(0, -1)}))}} >Delete</p>
+                        <p className='text-red-600 delete_button font-medium font-sora cursor-pointer' onClick={() => { setQuestion(prev => ({ ...prev, answerOptions: prev.answerOptions.slice(0, -1) })) }} >Delete</p>
                     </div>
                 </div>
                 {question.answerOptions.length > 1 ? <div className='flex mx-auto flex-col mt-5 gap-2'>
                     <label htmlFor="question" className='text-black font-semibold text-lg '>Correct Answer</label>
-                    <input type="text" name="question" id="" className='outline-1 h-10 outline-black/50 rounded-lg pl-4' onChange={(e) => { setQuestion({ ...question, correctAnswer: e.target.value }) }} placeholder='Eg : "1" or "2", Without Quotes' />
+                    <input type="text" name="question" id="" className='outline-1 h-10 outline-black/50 rounded-lg pl-4'
+                        onChange={
+                            (e) => {
+                                setQuestion({ ...question, correctAnswer: e.target.value })
+                            }}
+                        placeholder='Eg : "1" or "2", Without Quotes' />
                 </div> : <></>}
 
+                { Number(question.correctAnswer) > question.answerOptions.length || Number(question.correctAnswer) <= 0 ? <p className='text-lg font-sora text-red-600 mt-3'>Answer should be withine the range of the options</p> : <></>}
+
                 <div className='w-full mt-8'>
-                  {closeSubmit ?  <button className='w-full text-white bg-gray-300 font-sora py-3 rounded-lg' disabled>
+                    {closeSubmit ? <button className='w-full text-white bg-gray-300 font-sora py-3 rounded-lg' disabled>
                         Submit Question
                     </button> : <button className='w-full text-white bg-emerald-700 cursor-pointer font-sora py-3 rounded-lg' onClick={handleSubmit}>
                         Submit Question
